2c0e336d5e06df31fa8879b854921472113fe903
v <cryi@tutanota.com>
2024-09-15 00:09:30 +0200

    updated to lates eli-stream-extra

--- src/lpipe.c
+++ src/lpipe.c
@@ -4,14 +4,14 @@
 #include "pipe.h"
 
 static const struct luaL_Reg eliPipeExtra[] = {
-    {"pipe", eli_pipe},
-    {NULL, NULL},
+	{ "pipe", eli_pipe },
+	{ NULL, NULL },
 };
 
 int luaopen_eli_pipe_extra(lua_State *L)
 {
-    luaopen_eli_stream_extra(L);
-    lua_newtable(L);
-    luaL_setfuncs(L, eliPipeExtra, 0);
-    return 1;
+	luaopen_eli_stream_extra(L);
+	lua_newtable(L);
+	luaL_setfuncs(L, eliPipeExtra, 0);
+	return 1;
 }
--- src/pipe.c
+++ src/pipe.c
@@ -22,79 +22,79 @@
 #ifndef _WIN32
 static int closeonexec(int d)
 {
-    int fl = fcntl(d, F_GETFD);
-    if (fl != -1)
-        fl = fcntl(d, F_SETFD, fl | FD_CLOEXEC);
-    return fl;
+	int fl = fcntl(d, F_GETFD);
+	if (fl != -1)
+		fl = fcntl(d, F_SETFD, fl | FD_CLOEXEC);
+	return fl;
 }
 #endif
 
-int new_pipe(PIPE_DESCRIPTORS * descriptors) {
-    int fd[2];
+int new_pipe(PIPE_DESCRIPTORS *descriptors)
+{
+	int fd[2];
 #ifdef _WIN32
-    HANDLE ph[2];
-    int res = 0;
-    if (!CreatePipe(ph, ph + 1, 0, 0))
-        return -1;
-    res = SetHandleInformation(ph[0], HANDLE_FLAG_INHERIT, 0);
-    if (!res) {
-        return -1;
-    }
-    res = SetHandleInformation(ph[1], HANDLE_FLAG_INHERIT, 0);
-    if (!res) {
-        return -1;
-    }
+	HANDLE ph[2];
+	int res = 0;
+	if (!CreatePipe(ph, ph + 1, 0, 0))
+		return -1;
+	res = SetHandleInformation(ph[0], HANDLE_FLAG_INHERIT, 0);
+	if (!res) {
+		return -1;
+	}
+	res = SetHandleInformation(ph[1], HANDLE_FLAG_INHERIT, 0);
+	if (!res) {
+		return -1;
+	}
 
-    fd[0] = _open_osfhandle((intptr_t)ph[0], _O_RDONLY);
-    if (fd[0] == -1) {
-        return -1;
-    }
-    fd[1] = _open_osfhandle((intptr_t)ph[1], _O_WRONLY);
-    if (fd[1] == -1) {
-        close(fd[0]);
-        return -1;
-    }
+	fd[0] = _open_osfhandle((intptr_t)ph[0], _O_RDONLY);
+	if (fd[0] == -1) {
+		return -1;
+	}
+	fd[1] = _open_osfhandle((intptr_t)ph[1], _O_WRONLY);
+	if (fd[1] == -1) {
+		close(fd[0]);
+		return -1;
+	}
 #else
-    if (-1 == pipe(fd))
-        return -1;
-    closeonexec(fd[0]);
-    closeonexec(fd[1]);
+	if (-1 == pipe(fd))
+		return -1;
+	closeonexec(fd[0]);
+	closeonexec(fd[1]);
 #endif
-    descriptors->fd[0] = fd[0];
-    descriptors->fd[1] = fd[1];
-    return 0;
+	descriptors->fd[0] = fd[0];
+	descriptors->fd[1] = fd[1];
+	return 0;
 }
-
 
 static void new_eli_stream(lua_State *L, int fd, ELI_STREAM_KIND kind)
 {
-    ELI_STREAM *p = (ELI_STREAM *)lua_newuserdata(L, sizeof(ELI_STREAM));
-    switch (kind) {
-        case ELI_STREAM_R_KIND: 
-            luaL_getmetatable(L, ELI_STREAM_R_METATABLE);
-            break;
-        case ELI_STREAM_W_KIND: 
-            luaL_getmetatable(L, ELI_STREAM_W_METATABLE);
-            break;
-        case ELI_STREAM_RW_KIND:
-            luaL_getmetatable(L, ELI_STREAM_RW_METATABLE);
-            break;
-        default:
-            luaL_error(L, "[new_eli_stream] Invalid ELI_STREAM_KIND!");
-    }
-    lua_setmetatable(L, -2);
-    p->fd = fd;
-    p->nonblocking = 0;
-    p->closed = 0;
+	ELI_STREAM *p = eli_new_stream(L);
+	switch (kind) {
+	case ELI_STREAM_R_KIND:
+		luaL_getmetatable(L, ELI_STREAM_R_METATABLE);
+		break;
+	case ELI_STREAM_W_KIND:
+		luaL_getmetatable(L, ELI_STREAM_W_METATABLE);
+		break;
+	case ELI_STREAM_RW_KIND:
+		luaL_getmetatable(L, ELI_STREAM_RW_METATABLE);
+		break;
+	default:
+		luaL_error(L, "[new_eli_stream] Invalid ELI_STREAM_KIND!");
+	}
+	lua_setmetatable(L, -2);
+	p->fd = fd;
+	p->nonblocking = 0;
+	p->closed = 0;
 }
 
 /* -- in out/nil error */
 int eli_pipe(lua_State *L)
 {
-    PIPE_DESCRIPTORS descriptors;
-    if (new_pipe(&descriptors) == -1)
-        return push_error(L, NULL);
-    new_eli_stream(L, descriptors.fd[0], ELI_STREAM_R_KIND);
-    new_eli_stream(L, descriptors.fd[1], ELI_STREAM_W_KIND);
-    return 2;
+	PIPE_DESCRIPTORS descriptors;
+	if (new_pipe(&descriptors) == -1)
+		return push_error(L, NULL);
+	new_eli_stream(L, descriptors.fd[0], ELI_STREAM_R_KIND);
+	new_eli_stream(L, descriptors.fd[1], ELI_STREAM_W_KIND);
+	return 2;
 }
